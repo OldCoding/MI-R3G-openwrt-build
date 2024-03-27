30109591cbdd4d59f297181aaf6b0e44d27307f2
OldCoding <85047508+OldCoding@users.noreply.github.com>
2024-03-27 12:20:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -37,8 +37,7 @@ svn_export "master" "package/emortal" "package/emortal" "https://github.com/immo
 
 rm -rf package/helloworld/shadowsocksr-libev
 rm -rf package/openwrt-passwall-packages/shadowsocksr-libev
-svn_export "v5" "shadowsocksr-libev" "package/helloworld/shadowsocksr-libev" "https://github.com/sbwml/openwrt_helloworld"
-svn_export "v5" "shadowsocksr-libev" "package/openwrt-passwall-packages/shadowsocksr-libev" "https://github.com/sbwml/openwrt_helloworld"
+svn_export "v5" "shadowsocksr-libev" "package/shadowsocksr-libev" "https://github.com/sbwml/openwrt_helloworld"
 
 # turboacc 补丁
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
