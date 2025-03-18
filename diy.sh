555eecd9020faa6a5cebfc0e246b1bc9e3649f0c
OldCoding <85047508+OldCoding@users.noreply.github.com>
2025-03-18 21:38:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,8 +12,8 @@ svn_export() {
 	rm -rf "$TMP_DIR"
 }
 
-#rm -rf feeds/packages/lang/golang 
-#git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang 
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 rm -rf ./feeds/luci/applications/luci-app-smartdns
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/xray-core
